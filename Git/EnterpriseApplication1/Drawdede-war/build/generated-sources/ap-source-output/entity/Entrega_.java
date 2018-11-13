dd8db803549b890781e140d35b34f61064cc465e
xEDG <eduardogj96@gmail.com>
2018-11-13 16:59:57 +0100

    Setting up six

---
+++ Git/EnterpriseApplication1/Drawdede-war/build/generated-sources/ap-source-output/entity/Entrega_.java
@@ -0,0 +1,15 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-13T16:16:43")
+@StaticMetamodel(Entrega.class)
+public class Entrega_ { 
+
+    public static volatile SingularAttribute<Entrega, String> fechadeentrega;
+    public static volatile SingularAttribute<Entrega, String> id;
+    public static volatile SingularAttribute<Entrega, String> anotacion;
+
+}
