28bedfcb19edf11752263c744c76bb583005c1e8
xEDG <eduardogj96@gmail.com>
2018-11-13 16:19:29 +0100

    Setting up four

--- Git/EnterpriseApplication1/Drawdede-war/src/java/facade/SerieFacade.java
+++ Git/EnterpriseApplication1/Drawdede-war/src/java/facade/SerieFacade.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package beans;
+package facade;
 
 import entity.Serie;
 import javax.ejb.Stateless;
