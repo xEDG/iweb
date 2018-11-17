97f16426a92dbcd9b592458ecc1b704cfb3d568c
angelillo97 <angelillo97@gmail.com>
2018-11-17 17:49:16 +0100

    Filtros de Entrega

--- Git/Drawdede-cliente/src/java/managed/MisEntregasManagedBean.java
+++ Git/Drawdede-cliente/src/java/managed/MisEntregasManagedBean.java
@@ -67,6 +67,22 @@ public class MisEntregasManagedBean {
         this.entregas = entregas;
     }
 
+    public String getT1() {
+        return t1;
+    }
+
+    public void setT1(String t1) {
+        this.t1 = t1;
+    }
+
+    public String getT2() {
+        return t2;
+    }
+
+    public void setT2(String t2) {
+        this.t2 = t2;
+    }
+
     public void setResultadoBusqueda(List<Entrega> resultadoBusqueda) {
         this.resultadoBusqueda = resultadoBusqueda;
     }
